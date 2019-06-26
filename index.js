87ee49c9afaf6ba0f9b6de85ae8ce8a0751d5fdd
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-26 09:27:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function addToCart(item){
         itemName: [item],
         itemPrice:  Math.floor(Math.random() * Math.floor(100))
     }
-    cart.push(shopping.itemName)
-    return `${cart} has been added to your cart.`;
+    
+    return `${cart.push(shopping.itemName)} has been added to your cart.`;
 }
 
 
